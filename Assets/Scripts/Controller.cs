8432e429dc759df424f309259947b427cf857e80
Thomas <tom.pradal@aliceadsl.fr>
2020-09-25 00:27:16 +0200

    Nature kit used in scene. Scream sfx

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public List<AudioClip> ScreamSounds;
+
+    private AudioSource audioComp;
+
     private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioComp = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetBool("Walk", true);
+            animator.SetBool("Scream", true);
         }
-        else
-        {
-            animator.SetBool("Walk", false);
-        }
+    }
+
+    public void OnScream()
+    {
+        //TODO 1 function per scream, force anim stop by hand depending on sound length
+        audioComp.PlayOneShot(ScreamSounds[0]);
+        animator.SetBool("Scream", false);
     }
 }
